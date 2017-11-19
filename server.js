5b7dfda741669326ad4288f0e9e6ac0294edf4ea
sostenesg7 <sostenesg7@gmail.com>
2017-11-19 02:08:41 -0300

    init

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var server = app.listen(process.env.PORT || porta, ()=>{
 })
 
 app.get('/',(req, res)=>{
-	res.send('API de testes mod')
+	res.send('API de testes')
 })
 
 app.get('/grafico', (req, res) => {
