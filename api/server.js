8a0f457cf98520bff187671e6feb33273c30859e
Sóstenes Gomes de Souza <sostenesg7@gmail.com>
2017-11-25 00:17:24 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -37,7 +37,7 @@ app.post('/webhook', (req, res) => {
 
   // Parse the request body from the POST
   let body = req.body;
-
+  console.log(JSON.stringify(red.body))
   // Check the webhook event is from a Page subscription
   if (body.object === 'page') {
 
